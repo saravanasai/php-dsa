4a5330c76020059d4a908d5f08776ec205f4838b
saravana sai <saravanasai002@gmail.com>
2024-06-08 23:09:32 +0530

    Added delete node tree & height methods

--- BinaryTree/binaryTree.php
+++ BinaryTree/binaryTree.php
@@ -4,7 +4,6 @@ include_once "../commons/queue.php";
 
 class BinaryTree
 {
-
     public ?Node $rootNode;
     public ?Queue $levelQueue;
 
@@ -76,6 +75,72 @@ class BinaryTree
         }
     }
 
+    public function height(Node $fromNode = null): int
+    {
+        $x = 0;
+        $y = 0;
+
+        if ($fromNode) {
+            $x = $this->height($fromNode->left);
+            $y = $this->height($fromNode->right);
+            return $x > $y ? $x + 1 : $y + 1;
+        }
+
+        return $x + $y;
+    }
+
+    private function inPre(Node $node): Node
+    {
+
+        while ($node && $node->right) {
+            $node = $node->right;
+        }
+
+        return $node;
+    }
+
+    private function inSuc(Node $node): Node
+    {
+
+        while ($node && $node->left) {
+            $node = $node->left;
+        }
+
+        return $node;
+    }
+    public function delete(int $val, Node $node = null): Node|null
+    {
+
+        if (!$node) {
+            return null;
+        }
+
+        if ($val < $node->val) {
+            $node->left = $this->delete($val, $node->left);
+        } else if ($val > $node->val) {
+            $node->right = $this->delete($val, $node->right);
+        }
+
+        if ($val == $node->val) {
+            // handling no child senario leaf node
+            if (!$node->left && !$node->right) {
+                return null;
+            }
+
+            if ($this->height($node->left) > $this->height($node->right)) {
+
+                $preNode = $this->inPre($node->left);
+                $node->val = $preNode->val;
+                $node->left = $this->delete($preNode->val, $node->left);
+            } else {
+                $preNode = $this->inSuc($node->right);
+                $node->val = $preNode->val;
+                $node->right = $this->delete($preNode->val, $node->right);
+            }
+        }
+        return $node;
+    }
+
     private function searchRecursive(Node $node = null, int $val): int
     {
         if (!$node) {
@@ -115,13 +180,19 @@ class BinaryTree
 
 $binaryTree = new BinaryTree();
 
-$binaryTree->insert(50);
+$binaryTree->insert(12);
+$binaryTree->insert(6);
 $binaryTree->insert(20);
-$binaryTree->insert(30);
-$binaryTree->insert(10);
-$binaryTree->insert(60);
-$binaryTree->insert(100);
-$binaryTree->insert(90);
+$binaryTree->insert(3);
+$binaryTree->insert(8);
+$binaryTree->insert(16);
+$binaryTree->insert(40);
+$binaryTree->insert(2);
+$binaryTree->insert(4);
+echo "\n Height of binary tree:" . $binaryTree->height($binaryTree->rootNode) . "\n";
 $binaryTree->levelOrderTraversal($binaryTree->rootNode);
-echo "\nSearching for val[10]:\t" . $binaryTree->search(10);
-echo "\nSearching for val[90]:\t" . $binaryTree->search(90);
+echo "\nSearching for val[10]:\t" . $binaryTree->search(10) . "\n";
+echo "\nSearching for val[90]:\t" . $binaryTree->search(90) . "\n";
+$binaryTree->delete(6, $binaryTree->rootNode);
+echo "\n Height of binary tree:" . $binaryTree->height($binaryTree->rootNode) . "\n";
+$binaryTree->levelOrderTraversal($binaryTree->rootNode);
