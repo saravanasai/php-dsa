57d78634a5d0ab4898c10c3dd67add023f1db939
saravana sai <saravanasai002@gmail.com>
2024-10-16 09:45:04 +0530

    string casting for memo

--- Dynamic-Programming/Fibo.php
+++ Dynamic-Programming/Fibo.php
@@ -3,15 +3,15 @@
 
 function fibo($n, $map = []): int
 {
-    if (isset($map["$n"])) {
-        return $map["$n"];
+    if (isset($map[(string)$n])) {
+        return $map[(string)$n];
     }
     if ($n <= 2) {
         return 1;
     }
 
-    $map["$n"] = fibo($n - 1) + fibo($n - 2);
-    return $map["$n"];
+    $map[(string)$n] = fibo($n - 1) + fibo($n - 2);
+    return $map[(string)$n];
 }
 
 echo fibo(30);
